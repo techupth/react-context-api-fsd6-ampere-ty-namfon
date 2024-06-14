bc0b93f38e6184e863546bf11807fa86d8b1f126
Namfon <varisa.vb@hotmail.com>
2024-06-14 13:07:15 +0700

    feat: Add context API

--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,14 @@
+import { useContext } from "react";
+import { UserDataContext } from "../App";
+
 function AppHeader() {
+  const userContext = useContext(UserDataContext);
+  // console.log(userContext);
+
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {userContext.username}</h2>
     </div>
   );
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import products from "../data/products.js";
 
 function HomePage() {
   const navigate = useNavigate();
+
   return (
     <div>
       <div className="app-wrapper">
@@ -12,7 +13,7 @@ function HomePage() {
       <div className="product-list">
         {products.map((product) => {
           return (
-            <div className="product">
+            <div className="product" key={product.id}>
               <div className="product-preview">
                 <img
                   src="https://via.placeholder.com/250/250"
